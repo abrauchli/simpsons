6addec0ba1e383f05b4806115e2dd495d659f7bf
Andreas Brauchli <a.brauchli@elementarea.net>
2014-04-03 15:57:23 -1000

    Add first parser output

---
+++ data/season.js
@@ -0,0 +1,28 @@
+var seasons = [
+  {"season":1,years:"1989/90"},
+  {"season":2,years:"1990/91"},
+  {"season":3,years:"1991/92"},
+  {"season":4,years:"1992/93"},
+  {"season":5,years:"1993/94"},
+  {"season":6,years:"1994/95"},
+  {"season":7,years:"1995/96"},
+  {"season":8,years:"1996/97"},
+  {"season":9,years:"1997/98"},
+  {"season":10,years:"1998/99"},
+  {"season":11,years:"1999/00"},
+  {"season":12,years:"2000/01"},
+  {"season":13,years:"2001/02"},
+  {"season":14,years:"2002/03"},
+  {"season":15,years:"2003/04"},
+  {"season":16,years:"2004/05"},
+  {"season":17,years:"2005/06"},
+  {"season":18,years:"2006/07"},
+  {"season":19,years:"2007/08"},
+  {"season":20,years:"2008/09"},
+  {"season":21,years:"2009/10"},
+  {"season":22,years:"2010/11"},
+  {"season":23,years:"2011/12"},
+  {"season":24,years:"2012/13"},
+  {"season":25,years:"2013/14"}
+  //,{"season":26,years:"2014/15"}
+];
