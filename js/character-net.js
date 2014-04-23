20a4d9d85d1e63034f9ebcee1ec75e6a427291cc
Andreas Brauchli <a.brauchli@elementarea.net>
2014-04-22 22:02:31 -1000

    Add copyright

--- js/character-net.js
+++ js/character-net.js
@@ -1,6 +1,7 @@
 /* ---------------------------------------------------------------------------
    (c) Telefï¿½nica I+D, 2013
    Author: Paulo Villegas
+   (c) Andreas Brauchli, Tianli Mo, 2014 (Changes)
 
    This script is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
