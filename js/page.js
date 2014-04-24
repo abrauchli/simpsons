578dffd8816cfa9650a5a3a9aa0d31a8aa3adcf8
Andreas Brauchli <a.brauchli@elementarea.net>
2014-04-23 17:25:46 -1000

    Remove unnecessary fields

--- js/page.js
+++ js/page.js
@@ -7,12 +7,10 @@ var rowLabel = ['Homer Simpson']; // change to gene name or probe id
 var colLabel = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25']; // change to contrast name
 
 var characterFilter = {
-	"filter": 1,
 	"ageMin": 0,
 	"ageMax": 100,
 	"male": 1,
 	"female": 1,
-	"selectedIndex": -1,
 	"showSingle": false
 }
 
